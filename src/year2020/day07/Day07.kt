4bb9411a55f6f77ddd149827c8bdaa955182d3b1
Jorkoh <kohru95@gmail.com>
2021-12-14 23:31:13 +0100

    Day 14, 2021

---
+++ src/year2020/day07/Day07.kt
@@ -0,0 +1,25 @@
+package year2020.day07
+
+import utils.Solution
+
+fun main() {
+    with(Day07()){
+        test(::part1, 4, "test1")
+        test(::part2, 32, "test1")
+        test(::part2, 126, "test2")
+        solve()
+    }
+}
+
+private class Day07 : Solution{
+    override val day = 7
+    override val year = 2020
+
+    override fun part1(input: List<String>): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: List<String>): Any {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/year2021/day14/Day14.kt
@@ -0,0 +1,66 @@
+package year2021.day14
+
+import utils.Solution
+
+fun main() {
+    with(Day14()) {
+        test(::part1, 1588L)
+        test(::part2, 2188189693529L)
+        solve()
+    }
+}
+
+private class Day14 : Solution {
+    override val day = 14
+    override val year = 2021
+
+    override fun part1(input: List<String>): Any {
+        var pairCounts = input.first().windowed(2).associate { (it[0] to it[1]) to 1L }
+        val rules = input.drop(2).associate { (it[0] to it[1]) to it[6] }
+
+        repeat(10) {
+            pairCounts = buildMap {
+                pairCounts.forEach { (pair, count) ->
+                    merge(pair.first to rules[pair]!!, count, Long::plus)
+                    merge(rules[pair]!! to pair.second, count, Long::plus)
+                }
+            }
+        }
+
+        val charCounts = buildMap<Char, Long> {
+            pairCounts.forEach { (first, second), count ->
+                merge(first, count, Long::plus)
+                merge(second, count, Long::plus)
+            }
+            merge(input.first().first(), 1, Long::plus)
+            merge(input.first().last(), 1, Long::plus)
+        }
+
+        return charCounts.toList().sortedBy { it.second }.let { it.last().second - it.first().second } / 2
+    }
+
+    override fun part2(input: List<String>): Any {
+        var pairCounts = input.first().windowed(2).associate { (it[0] to it[1]) to 1L }
+        val rules = input.drop(2).associate { (it[0] to it[1]) to it[6] }
+
+        repeat(40) {
+            pairCounts = buildMap {
+                pairCounts.forEach { (pair, count) ->
+                    merge(pair.first to rules[pair]!!, count, Long::plus)
+                    merge(rules[pair]!! to pair.second, count, Long::plus)
+                }
+            }
+        }
+
+        val charCounts = buildMap<Char, Long> {
+            pairCounts.forEach { (first, second), count ->
+                merge(first, count, Long::plus)
+                merge(second, count, Long::plus)
+            }
+            merge(input.first().first(), 1, Long::plus)
+            merge(input.first().last(), 1, Long::plus)
+        }
+
+        return charCounts.toList().sortedBy { it.second }.let { it.last().second - it.first().second } / 2
+    }
+}
