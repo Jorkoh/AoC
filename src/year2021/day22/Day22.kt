6a3db4418169df246ccf7b51ee53a009ee2c24c4
Jorkoh <kohru95@gmail.com>
2021-12-23 20:33:55 +0100

    Day 22, 2021

--- src/year2021/day22/Day22.kt
+++ src/year2021/day22/Day22.kt
@@ -1,8 +1,6 @@
 package year2021.day22
 
 import utils.Solution
-import java.lang.Long.max
-import java.lang.Long.min
 
 fun main() {
     with(Day22()) {
@@ -20,29 +18,19 @@ private class Day22 : Solution {
         On, Off
     }
 
-    private data class Cuboid(
-        val x: LongRange,
-        val y: LongRange,
-        val z: LongRange
-    ) {
+    private data class Cuboid(val x: LongRange, val y: LongRange, val z: LongRange) {
         val volume = if (x.isEmpty() || y.isEmpty() || z.isEmpty()) {
             0L
         } else {
-            (x.last - x.first) * (y.last - y.first) * (z.last - z.first)
+            (x.last - x.first + 1) * (y.last - y.first + 1) * (z.last - z.first + 1)
         }
     }
 
     private infix fun LongRange.overlaps(that: LongRange) = first <= that.last && last >= that.first
 
-    private infix fun LongRange.contains(that: LongRange) = first <= that.first && last >= that.last
-
     // NOTE: this code is SUS (ඞ)
     private infix fun Cuboid.overlaps(that: Cuboid) =
         x overlaps that.x && y overlaps that.y && z overlaps that.z
-
-    // NOTE: this code is also SUS (ඞ)
-    private infix fun Cuboid.contains(that: Cuboid) =
-        x contains that.x && y contains that.y && z contains that.z
 
     override fun part1(input: List<String>): Any {
         val operations = input.map { line ->
@@ -87,59 +75,53 @@ private class Day22 : Solution {
         val onZones = mutableListOf<Cuboid>()
         operations.forEach { (type, newZone) ->
             val affectedZones = onZones.filter { it overlaps newZone }
-            val newZones = affectedZones.map { zone -> simplify(newZone, zone) }.flatten()
-
+            val newZones = affectedZones.flatMap { zone -> simplify(newZone, zone) }
             onZones.removeAll(affectedZones)
             onZones.addAll(newZones)
-            println("Zones ${onZones.size}")
             if (type == OperationType.On) onZones.add(newZone)
         }
 
         return onZones.sumOf { it.volume }
     }
 
-    // Transform the zones depending on newZone to create new zones "around" it
-    private fun simplify(newZone: Cuboid, zone: Cuboid): List<Cuboid> {
-        val xRanges = buildList {
-            if (newZone.x.first > zone.x.first) {
-                add(zone.x.first until newZone.x.first)
+    private fun simplify(newZone: Cuboid, zone: Cuboid): MutableList<Cuboid> {
+        val slice = buildList {
+            // X slices
+            if (newZone.x.first in zone.x.drop(1))
+                add(Cuboid(x = zone.x.first until newZone.x.first, y = zone.y, z = zone.z))
+            if (newZone.x.last in zone.x.toList().dropLast(1))
+                add(Cuboid(x = (newZone.x.last + 1)..zone.x.last, y = zone.y, z = zone.z))
+            // Y slices
+            if (newZone.y.first in zone.y.drop(1))
+                add(Cuboid(x = zone.x, y = zone.y.first until newZone.y.first, z = zone.z))
+            if (newZone.y.last in zone.y.toList().dropLast(1))
+                add(Cuboid(x = zone.x, y = (newZone.y.last + 1)..zone.y.last, z = zone.z))
+            // Z slices
+            if (newZone.z.first in zone.z.drop(1))
+                add(Cuboid(x = zone.x, y = zone.y, z = zone.z.first until newZone.z.first))
+            if (newZone.z.last in zone.z.toList().dropLast(1))
+                add(Cuboid(x = zone.x, y = zone.y, z = (newZone.z.last + 1)..zone.z.last))
+        }.maxByOrNull { it.volume } ?: return mutableListOf()
+
+        val remaining = when {
+            slice.x != zone.x -> { // X slice
+                val remainingX = if (slice.x.first == zone.x.first) (slice.x.last + 1)..zone.x.last
+                else zone.x.first until slice.x.first
+                Cuboid(x = remainingX, y = zone.y, z = zone.z)
             }
-            if (newZone.x.last + 1 < zone.x.last) {
-                add((newZone.x.last + 1)..zone.x.last)
+            slice.y != zone.y -> { // Y slice
+                val remainingY = if (slice.y.first == zone.y.first) (slice.y.last + 1)..zone.y.last
+                else zone.y.first until slice.y.first
+                Cuboid(x = zone.x, y = remainingY, z = zone.z)
             }
-            if (newZone.x.first > zone.x.first || newZone.x.last + 1 < zone.x.last) {
-                add(max(zone.x.first, newZone.x.first)..min(zone.x.last, newZone.x.last))
+            slice.z != zone.z -> { // Z slice
+                val remainingZ = if (slice.z.first == zone.z.first) (slice.z.last + 1)..zone.z.last
+                else zone.z.first until slice.z.first
+                Cuboid(x = zone.x, y = zone.y, z = remainingZ)
             }
-        }
-        val yRanges = buildList {
-            if (newZone.y.first > zone.y.first) {
-                add(zone.y.first until newZone.y.first)
-            }
-            if (newZone.y.last + 1 < zone.y.last) {
-                add((newZone.y.last + 1)..zone.y.last)
-            }
-            if (newZone.y.first > zone.y.first || newZone.y.last + 1 < zone.y.last) {
-                add(max(zone.y.first, newZone.y.first)..min(zone.y.last, newZone.y.last))
-            }
-        }
-        val zRanges = buildList {
-            if (newZone.z.first > zone.z.first) {
-                add(zone.z.first until newZone.z.first)
-            }
-            if (newZone.z.last + 1 < zone.z.last) {
-                add((newZone.z.last + 1)..zone.z.last)
-            }
-            if (newZone.z.first > zone.z.first || newZone.z.last + 1 < zone.z.last) {
-                add(max(zone.z.first, newZone.z.first)..min(zone.z.last, newZone.z.last))
-            }
+            else -> throw IllegalStateException()
         }
 
-        return xRanges.map { xRange ->
-            yRanges.map { yRange ->
-                zRanges.map { zRange ->
-                    Cuboid(xRange, yRange, zRange)
-                }
-            }
-        }.flatten().flatten().filterNot { it.x == newZone.x && it.y == newZone.y && it.z == newZone.z }
+        return simplify(newZone, remaining).apply { add(slice) }
     }
 }
