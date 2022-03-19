100a6ae0a145a092c03e58fd2a19b4d6e8751d04
Jorkoh <kohru95@gmail.com>
2022-03-19 15:40:42 +0100

    Day 08, 2015

---
+++ src/year2015/day08/Day08.kt
@@ -0,0 +1,82 @@
+package year2015.day08
+
+import utils.Solution
+
+fun main() {
+    with(Day08()) {
+        solve()
+    }
+}
+
+private class Day08 : Solution {
+    override val day = 8
+    override val year = 2015
+
+    override fun part1(input: List<String>): Any {
+        return input.sumOf { line ->
+            // Count the non-printable characters
+            var count = 2
+            var i = 1
+            var previousBackslash = false
+
+            while (i < line.lastIndex) {
+                val c = line[i]
+                var currentBackslash = false
+
+                when (c) {
+                    '\\' -> {
+                        if (previousBackslash) count += 1 else currentBackslash = true
+                    }
+                    '"' -> {
+                        if (previousBackslash) count += 1 else throw IllegalStateException()
+                    }
+                    'x' -> {
+                        if (previousBackslash) {
+                            count += 3
+                            i += 2
+                        }
+                    }
+                }
+
+                i += 1
+                previousBackslash = currentBackslash
+            }
+
+            count
+        }
+    }
+
+    override fun part2(input: List<String>): Any {
+        return input.sumOf { line ->
+            // Count the non-printable characters after "encoding" the string
+            var count = 4
+            var i = 1
+            var previousBackslash = false
+
+            while (i < line.lastIndex) {
+                val c = line[i]
+                var currentBackslash = false
+
+                when (c) {
+                    '\\' -> {
+                        if (previousBackslash) count += 2 else currentBackslash = true
+                    }
+                    '"' -> {
+                        if (previousBackslash) count += 2 else throw IllegalStateException()
+                    }
+                    'x' -> {
+                        if (previousBackslash) {
+                            count += 1
+                            i += 2
+                        }
+                    }
+                }
+
+                i += 1
+                previousBackslash = currentBackslash
+            }
+
+            count
+        }
+    }
+}
