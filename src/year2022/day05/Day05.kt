23fa59b2ea5ef72118be6271473c7714adcaa974
Jorkoh <kohru95@gmail.com>
2022-12-05 16:58:26 +0100

    Clean up day 05, 2022 a bit

--- src/year2022/day05/Day05.kt
+++ src/year2022/day05/Day05.kt
@@ -15,42 +15,39 @@ private class Day05 : Solution {
     override val year = 2022
 
     override fun part1(input: List<String>): Any {
-        val stacks = parseStacks(input)
-        input.dropWhile { it.firstOrNull() != 'm' }.map(::parse).forEach { (count, from, to) ->
+        val stacks = input.parseStacks()
+        input.dropWhile { it.firstOrNull() != 'm' }.map(::parseInstruction).forEach { (count, from, to) ->
             repeat(count) {
-                val removed = stacks[from - 1].removeLast()
-                stacks[to - 1].addLast(removed)
+                val toMove = stacks[from - 1].removeLast()
+                stacks[to - 1].addLast(toMove)
             }
         }
 
-        return stacks.joinToString(separator = "") { it.last().toString() }
+        return stacks.map { it.last() }.joinToString(separator = "")
     }
 
     override fun part2(input: List<String>): Any {
-        val stacks = parseStacks(input)
-        input.dropWhile { it.firstOrNull() != 'm' }.map(::parse).forEach { (count, from, to) ->
-            val removed = stacks[from - 1].takeLast(count)
+        val stacks = input.parseStacks()
+        input.dropWhile { it.firstOrNull() != 'm' }.map(::parseInstruction).forEach { (count, from, to) ->
+            val toMove = stacks[from - 1].takeLast(count)
             repeat(count) { stacks[from - 1].removeLast() }
-            stacks[to - 1].addAll(removed)
+            stacks[to - 1].addAll(toMove)
         }
 
-        return stacks.joinToString(separator = "") { it.last().toString() }
+        return stacks.map { it.last() }.joinToString(separator = "")
     }
 
-    private fun parseStacks(input: List<String>): List<ArrayDeque<Char>> {
-        val rowCount = input.indexOfFirst { it.isBlank() } - 1
-        val stacks = List((input[rowCount - 1].length + 1) / 4) { ArrayDeque<Char>() }
-
-        input.take(rowCount).forEach { l ->
-            stacks.forEachIndexed { i, stack ->
-                val c = l.getOrNull(i * 4 + 1) ?: ' '
-                if (c != ' ') stack.addFirst(c)
+    private fun List<String>.parseStacks(): List<ArrayDeque<Char>> {
+        return List((first().length + 1) / 4) { ArrayDeque<Char>() }.also { stacks ->
+            takeWhile { it[1] != '1' }.forEach { l ->
+                stacks.forEachIndexed { i, stack ->
+                    val c = l.getOrNull(i * 4 + 1) ?: ' '
+                    if (c != ' ') stack.addFirst(c)
+                }
             }
         }
-
-        return stacks
     }
 
     private val regex = Regex("""\D+(\d+)\D+(\d+)\D+(\d+)""")
-    private fun parse(line: String) = regex.find(line)!!.groupValues.drop(1).map(String::toInt)
+    private fun parseInstruction(line: String) = regex.find(line)!!.groupValues.drop(1).map(String::toInt)
 }
