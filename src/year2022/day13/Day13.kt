7735673fcfeda36040913ebd99cf2858f8b7b875
Jorkoh <kohru95@gmail.com>
2022-12-16 10:46:15 +0100

    Day 13, 2022

--- src/year2022/day13/Day13.kt
+++ src/year2022/day13/Day13.kt
@@ -7,8 +7,9 @@ import year2022.day13.Day13.PacketStuff.PacketNumber
 fun main() {
     with(Day13()) {
         testFile(::part1, 13)
-//        testFile(::part2, Unit)
-//        solve()
+        testFile(::part2, 140)
+        solve()
+        benchmark()
     }
 }
 
@@ -16,26 +17,25 @@ class Day13 : Solution() {
     override val day = 13
     override val year = 2022
 
-    private sealed interface PacketStuff {
-        data class Packet(val stuff: MutableList<PacketStuff> = mutableListOf()) : PacketStuff
-        data class PacketNumber(val number: Int) : PacketStuff
-    }
-
     override fun part1(): Any {
         val pairs = input.chunked(3) { (a, b, _) -> a.asPacket() to b.asPacket() }
         return pairs.withIndex().sumOf { (i, pair) ->
-            if (pair.isInRightOrder()) {
-                println("right order: ${pair.first} - ${pair.second}")
-                i
-            } else {
-                println("wrong order: ${pair.first} - ${pair.second}")
-                0
+            when (pair.first.compareTo(pair.second)) {
+                -1 -> i + 1
+                1 -> 0
+                else -> throw IllegalStateException() // Both are the same size, can't happen
             }
         }
     }
 
     override fun part2(): Any {
-        return Unit
+        val dividerOne = Packet(mutableListOf(PacketNumber(2).asPacket()))
+        val dividerTwo = Packet(mutableListOf(PacketNumber(6).asPacket()))
+        val packets = input.filter(String::isNotBlank).map { it.asPacket() }.toMutableList().apply {
+            add(dividerOne)
+            add(dividerTwo)
+        }.sorted()
+        return (packets.indexOf(dividerOne) + 1) * (packets.indexOf(dividerTwo) + 1)
     }
 
     private fun String.asPacket(): Packet = Packet().apply {
@@ -56,7 +56,42 @@ class Day13 : Solution() {
         }
     }
 
-    private fun Pair<Packet, Packet>.isInRightOrder(): Boolean {
-        return true
+    private sealed interface PacketStuff {
+
+        data class PacketNumber(val number: Int) : PacketStuff {
+            fun asPacket() = Packet().also { p -> p.stuff.add(this) }
+        }
+
+        data class Packet(val stuff: MutableList<PacketStuff> = mutableListOf()) : PacketStuff, Comparable<Packet> {
+            // -1 if first is smaller, 1 if first is bigger, 0 if the same
+            override fun compareTo(other: Packet): Int {
+                var i = 0
+                while (i < this.stuff.size && i < other.stuff.size) {
+                    val a = this.stuff[i]
+                    val b = other.stuff[i]
+                    val order = when {
+                        a is PacketNumber && b is PacketNumber -> {
+                            when {
+                                a.number < b.number -> -1
+                                a.number > b.number -> 1
+                                else -> 0
+                            }
+                        }
+                        a is Packet && b is Packet -> a.compareTo(b)
+                        a is Packet && b is PacketNumber -> a.compareTo(b.asPacket())
+                        a is PacketNumber && b is Packet -> a.asPacket().compareTo(b)
+                        else -> throw IllegalStateException()
+                    }
+                    if (order != 0) return order
+                    i++
+                }
+                // If we got here one of the packets run out
+                return when {
+                    this.stuff.size < other.stuff.size -> -1
+                    this.stuff.size > other.stuff.size -> 1
+                    else -> 0
+                }
+            }
+        }
     }
 }
