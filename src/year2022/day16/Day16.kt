e25210a8338268740af7b7fddcc948750f703ef7
Jorkoh <kohru95@gmail.com>
2022-12-18 22:09:10 +0100

    Day 16, 2022

--- src/year2022/day16/Day16.kt
+++ src/year2022/day16/Day16.kt
@@ -5,9 +5,8 @@ import utils.Solution
 fun main() {
     with(Day16()) {
         testFile(::part1, 1651)
-//        testFile(::part2, Unit)
-        solve()
-        benchmark()
+        testFile(::part2, 1707)
+        solve() // TODO part 2 takes 8 minutes to run as it is, shouldn't be hard to improve
     }
 }
 
@@ -17,38 +16,58 @@ class Day16 : Solution() {
 
     private val regex = Regex("""Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? (.+)""")
 
-    private data class Tunnel(val towardsId: String, val cost: Int)
+    private data class Tunnel(val toId: String, val cost: Int)
     private data class Room(val id: String, val flow: Int, val tunnels: List<Tunnel>)
 
     // The ones that have a flow rate of 0 are never worth turning
     override fun part1(): Any {
-        val allRooms = input.map { l ->
-            val (id, flowRaw, tunnelsRaw) = regex.find(l)!!.groupValues.drop(1)
-            Room(
-                id = id,
-                flow = flowRaw.toInt(),
-                tunnels = tunnelsRaw.split(", ").map { id -> Tunnel(id, 1) }
-            )
-        }
-
+        val allRooms = parseRooms()
         val withValve = allRooms.filter { it.flow > 0 }.map { it.id }.toMutableSet()
         val start = allRooms.first { it.id == "AA" }.connectTo(withValve, allRooms)
-        val relevantConnected = allRooms.filter { it.id in withValve }.map { room ->
+        val relevantConnected = allRooms.filter { it.flow > 0 }.map { room ->
             room.connectTo(withValve, allRooms)
         }
+
         return start.tunnels.maxOf { tunnel ->
-            val score = relevantConnected.bestRelease(
-                currentId = tunnel.towardsId,
+            relevantConnected.bestScoreAlone(
+                currentId = tunnel.toId,
                 timeRemaining = 30 - tunnel.cost,
                 scoreSoFar = 0,
                 unopenedVales = withValve
             )
-            score
         }
     }
 
     override fun part2(): Any {
-        return Unit
+        val allRooms = parseRooms()
+        val withValve = allRooms.filter { it.flow > 0 }.map { it.id }.toMutableSet()
+        val start = allRooms.first { it.id == "AA" }.connectTo(withValve, allRooms)
+        val relevantConnected = allRooms.filter { it.flow > 0 }.map { room ->
+            room.connectTo(withValve, allRooms)
+        }
+
+        return start.tunnels.maxOf { tunnelA ->
+            start.tunnels.filter { it.toId != tunnelA.toId }.maxOf { tunnelB ->
+                relevantConnected.bestScoreWithHelper(
+                    currentIdA = tunnelA.toId,
+                    currentIdB = tunnelB.toId,
+                    timeA = 26 - tunnelA.cost,
+                    timeB = 26 - tunnelB.cost,
+                    scoreSoFarA = 0,
+                    scoreSoFarB = 0,
+                    unopenedVales = withValve
+                )
+            }
+        }
+    }
+
+    private fun parseRooms() = input.map { l ->
+        val (id, flowRaw, tunnelsRaw) = regex.find(l)!!.groupValues.drop(1)
+        Room(
+            id = id,
+            flow = flowRaw.toInt(),
+            tunnels = tunnelsRaw.split(", ").map { id -> Tunnel(id, 1) }
+        )
     }
 
     private fun Room.connectTo(targetIds: Set<String>, rooms: List<Room>): Room {
@@ -57,8 +76,8 @@ class Day16 : Solution() {
         val frontier = ArrayDeque(listOf(this))
         while (frontier.isNotEmpty()) {
             val current = frontier.removeFirst()
-            current.tunnels.filter { it.towardsId !in idToCost }.forEach { tunnel ->
-                val newRoom = rooms.first { room -> room.id == tunnel.towardsId }
+            current.tunnels.filter { it.toId !in idToCost }.forEach { tunnel ->
+                val newRoom = rooms.first { room -> room.id == tunnel.toId }
                 idToCost[newRoom.id] = idToCost[current.id]!! + 1
                 frontier.addLast(newRoom)
             }
@@ -71,30 +90,80 @@ class Day16 : Solution() {
     }
 
     // Find best score with DFS
-    private fun List<Room>.bestRelease(
+    private fun List<Room>.bestScoreAlone(
         currentId: String,
         timeRemaining: Int,
         scoreSoFar: Int,
-        // Make this a mutable list and remove and re-add to improve speed
         unopenedVales: MutableSet<String>
     ): Int {
         if (timeRemaining <= 0) return scoreSoFar // Time run out in the tunnel or while turning this valve
 
         val current = first { it.id == currentId }
         val newScore = scoreSoFar + (timeRemaining - 1) * current.flow
-        val options = current.tunnels.filter { it.towardsId in unopenedVales }
+        val options = current.tunnels.filter { it.toId in unopenedVales }
 
         if (options.isEmpty()) return newScore // No valves left to turn
 
         return options.maxOf { tunnel ->
-            bestRelease(
-                currentId = tunnel.towardsId,
+            bestScoreAlone(
+                currentId = tunnel.toId,
                 timeRemaining = timeRemaining - 1 - tunnel.cost,
                 scoreSoFar = newScore,
                 unopenedVales = unopenedVales.apply { remove(currentId) }
             ).also {
                 unopenedVales.add(currentId)
             }
+        }
+    }
+
+    // Find best score with DFS
+    private fun List<Room>.bestScoreWithHelper(
+        currentIdA: String, // Walking towards
+        currentIdB: String, // Walking towards
+        timeA: Int, // When A arrives
+        timeB: Int, // When B arrives
+        scoreSoFarA: Int, // Score accumulated by A
+        scoreSoFarB: Int, // Score accumulated by B
+        unopenedVales: MutableSet<String> // Remaining valves
+    ): Int {
+        // Neither has time to turn a valve (negative means time ran out in tunnel)
+        if (timeA <= 0 && timeB <= 0) return scoreSoFarA + scoreSoFarB
+
+        // Process the first one to arrive, if tie main
+        val turnA = timeA >= timeB
+        val currentId = if (turnA) currentIdA else currentIdB
+        val current = first { it.id == currentId }
+        val scoreSoFar = if (turnA) scoreSoFarA else scoreSoFarB
+        val time = if (turnA) timeA else timeB
+        val newScore = scoreSoFar + (time - 1) * current.flow
+        val options = current.tunnels.filter { t ->
+            t.toId in unopenedVales && t.toId != currentIdA && t.toId != currentIdB
+        }
+
+        return if (options.isNotEmpty()) {
+            options.maxOf { tunnel ->
+                bestScoreWithHelper(
+                    currentIdA = if (turnA) tunnel.toId else currentIdA,
+                    currentIdB = if (!turnA) tunnel.toId else currentIdB,
+                    timeA = if (turnA) time - 1 - tunnel.cost else timeA,
+                    timeB = if (!turnA) time - 1 - tunnel.cost else timeB,
+                    scoreSoFarA = if (turnA) newScore else scoreSoFarA,
+                    scoreSoFarB = if (!turnA) newScore else scoreSoFarB,
+                    unopenedVales = unopenedVales.apply { remove(currentId) }
+                ).also { unopenedVales.add(currentId) }
+            }
+        } else {
+            // No valves left to turn but may still need to process 
+            // the other so drop this one to negative time :^)
+            return bestScoreWithHelper(
+                currentIdA = currentIdA,
+                currentIdB = currentIdB,
+                timeA = if (turnA) -99999 else timeA,
+                timeB = if (!turnA) -99999 else timeB,
+                scoreSoFarA = if (turnA) newScore else scoreSoFarA,
+                scoreSoFarB = if (!turnA) newScore else scoreSoFarB,
+                unopenedVales = unopenedVales.apply { remove(currentId) }
+            ).also { unopenedVales.add(currentId) }
         }
     }
 }
